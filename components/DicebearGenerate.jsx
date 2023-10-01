25ff6dbf9f185e3d82a6f2549638f1f433c4af1d
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-10-01 21:02:48 +0200

    used robohash for avatars generations

--- components/DicebearGenerate.jsx
+++ components/DicebearGenerate.jsx
@@ -1,20 +1,39 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+export const DicebearGenerate = () => {
+  const [avatars, setavatars] = useState([]);
+  useEffect(() => {
+    const contributors = [];
+    const roboHashBaseUrl = 'https://robohash.org/';
+    for (let i = 1; i <= 50; i++) {
+      contributors.push(`contributer${i}`);
+    }
 
-import React from 'react';
-import { createAvatar } from '@dicebear/core';
-import { lorelei } from '@dicebear/collection';
+    const GenerateAvatars = async () => {
+      try {
+        const avatarUrls = await Promise.all(
+          contributors.map(async (contributor) => {
+            const avatarUrl = `${roboHashBaseUrl}${contributor}.png`;
+            return avatarUrl; 
+          })
+        );
+        setavatars(avatarUrls)
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-const DicebearAvatar = () => {
-  const avatar = createAvatar(lorelei, {
-    seed: 'John Doe',
-    // ... other options
-  });
+    GenerateAvatars();
+  }, []);
 
-  // const svg = avatar.toString();
-  return(
-  // <div dangerouslySetInnerHTML={{ __html: svg }} />
-    <div></div>
+  return (
+    <div className='flex flex-wrap max-w-full justify-center'>
+      {avatars?.map((avatar) => (
+        <div key={avatar} className='w-24 h-24 ml-[-20px] rounded-full border border-2 bg-gray-400 cursor-pointer hover:opacity-90 rtansition-all duration-300 ease-in-out'>
+          <Image alt='avatar' width='100' height='100' src={avatar} />
+        </div>
+      ))}
+    </div>
   )
-
 };
-
-export default DicebearAvatar;
